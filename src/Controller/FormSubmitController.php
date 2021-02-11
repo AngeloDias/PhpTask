68db8e1f82f0955663cf2493bde12276a11e79d3
angelo.santana <angelossdias@gmail.com>
2021-02-11 11:17:20 -0300

    feat: updating routing and homepage

--- src/Controller/FormSubmitController.php
+++ src/Controller/FormSubmitController.php
@@ -8,13 +8,12 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class FormSubmitController extends AbstractController
 {
-    /**
-     * @Route("/form/submit", name="form_submit")
-     */
     public function index(): Response
     {
-        return $this->render('form_submit/index.html.twig', [
-            'controller_name' => 'FormSubmitController',
-        ]);
+        return $this->render('form_submit/index.html.twig',
+            [
+                'controller_name' => 'FormSubmitController'
+            ]
+        );
     }
 }
