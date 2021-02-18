15d392a3ec9075946605977ae8cc066772e53016
angelo.santana <angelossdias@gmail.com>
2021-02-18 12:38:31 -0300

    feat: adding materialize style and doing route to work

--- src/Controller/ShowImcResultController.php
+++ src/Controller/ShowImcResultController.php
@@ -10,5 +10,26 @@ class ShowImcResultController extends AbstractController
 {
     public function showResult(Request $request): Response
     {
+        $peso = $request->request->get('peso');
+        $altura = $request->request->get('altura');
+        $altura = $altura / 100;
+        $imc = $peso / ($altura * $altura);
+        $msg = '';
+
+        if($imc < 18.5) {
+            $msg = 'Tá bom de ganhar uns pesos. Vai puxar ferro, fazer natação e comer muito!';
+        } elseif ($imc > 18.4 && $imc < 25) {
+            $msg = 'Olha, não tá mau, mas pode ficar. Continua se alimentando direito e treinando.';
+        } elseif ($imc > 24.9 && $imc <= 30) {
+            $msg = 'Eita! Vai dar ruim! Começa a reeducar a alimentação e treinar leve.';
+        } else {
+            $msg = 'Você vai explodir, miséra! Corre para uma nutricionista e treinador físico!';
+        }
+
+        return $this->render('form_submit/imc_result.html.twig',
+        [
+            'imc' => $imc,
+            'incentivo' => $msg
+        ]);
     }
 }
